a3cdef784eee3642b7213a0f96372175813a963e
Addison Jones <adjones@student.neumont.edu>
2017-08-11 13:20:25 -0600

    fidleing with UI

--- War of the Immortals/War of the Immortals/Models/Instruments/Mechanism.cs
+++ War of the Immortals/War of the Immortals/Models/Instruments/Mechanism.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 using War_of_the_Immortals.Interfaces;
 namespace War_of_the_Immortals.models.Instruments
 {
-    public class Mechanism :Card, ILowerField
+    public class Mechanism : Card, ILowerField, IEffect
     {
+        public void Effect()
+        {
+            throw new NotImplementedException();
+        }
+        public long Effect(params int[] d)
+        {
+            throw new NotImplementedException();
+        }
+        public string Effect(params string[] s)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
